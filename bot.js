6f56e9b634c811faf9a925536f21294eba0e93bb
galaxy2211 <56916688+galaxy2211@users.noreply.github.com>
2019-11-17 17:36:37 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,9 +9,9 @@ bot.on('message', message =>{
     var msg = message.content;
     var prefix = "/" //The text before commands, you can put anything that you prefer
 
-    if(message.author.id != "482721587771080704" && message.channel.id === "634023000441749510"){
+    if(message.author.id != "482721587771080704" && message.channel.id === "63402300770441749510"){
         if(msg.startsWith('/', 0)){
-            if(msg === prefix + "bang" && message.channel.id === "634023000441749510"){
+            if(msg === prefix + "bang" && message.channel.id === "63402300770441749510"){
                 message.channel.send('BOOM!!!!!') // Sends a message to the channel, with the content
             }
             else{
