34ec3ddcd21cf446db03f272ba42b58a166fbd61
galaxy2211 <56916688+galaxy2211@users.noreply.github.com>
2019-11-17 08:53:04 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,11 +7,11 @@ bot.on('message', message =>{
     // Variables
     var sender = message.author; //The person who sent the message
     var msg = message.content;
-    var prefix = ">" //The text before commands, you can put anything that you prefer
+    var prefix = "/" //The text before commands, you can put anything that you prefer
 
-    if(message.author.id != "381443488392413184" && message.channel.id === "381397780385628181"){
-        if(msg.startsWith('>', 0)){
-            if(msg === prefix + "bang" && message.channel.id === "381397780385628181"){
+    if(message.author.id != "482721587771080704" && message.channel.id === "634023000441749510"){
+        if(msg.startsWith('/', 0)){
+            if(msg === prefix + "bang" && message.channel.id === "634023000441749510"){
                 message.channel.send('BOOM!!!!!') // Sends a message to the channel, with the content
             }
             else{
